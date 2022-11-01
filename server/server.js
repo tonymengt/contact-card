fcc7595040425f3e46477d99edf5a479840fdb9a
Tmeng <tonymengt@gmail.com>
2022-10-31 20:15:16 -0400

    enabled server from the client server architectural pattern

---
+++ server/server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.static('../client/'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+
+
+require('./routes/htmlRoutes')(app);
+
+app.listen(PORT, function() {
+    console.log(`now listent to port: ${PORT}`);
+});
