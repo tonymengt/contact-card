f14015b8fe67d812db8b597b5c1ad88a0944f12a
Tmeng <tonymengt@gmail.com>
2022-10-31 20:54:43 -0400

    enabled client-server and added concurrently

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 app.use(express.static('../client/'));
 app.use(express.urlencoded({extended: true}));
