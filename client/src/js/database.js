66455d8d4a07b73e367fa6574e7633807c9bd271
Tmeng <tonymengt@gmail.com>
2022-11-04 13:34:06 -0400

    added read and create method

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,4 +1,5 @@
 import { openDB } from 'idb';
+import { async } from 'regenerator-runtime';
 import 'regenerator-runtime/runtime';
 
 export const initdb = async () => {
@@ -14,3 +15,45 @@ export const initdb = async () => {
         }
     })
 }
+
+export const getDB = async () => {
+    console.log('GET from the database');
+
+    // create a connection to the indexedDB database and the version we want to use.
+    const contactDB = await openDB('contact_db', 1);
+
+    // create new transaction and specify the store and data privileges
+    const tx = contactDB.transaction('contacts', 'readonly');
+
+    // open up the desired object store
+    const store = tx.objectStore('contacts');
+
+    // use the .getall() method to get all data in the database
+    const request = store.getAll();
+
+    // get confirmation of the request
+    const result = await request;
+    console.log('results.value', result);
+    return result;
+};
+
+// Export a function we will use to POST to the database.
+export const postDb = async (name, email, phone, profile) => {
+    console.log('POST to the database');
+  
+    // Create a connection to the database and specify the version we want to use.
+    const contactDb = await openDB('contact_db', 1);
+  
+    // Create a new transaction and specify the store and data privileges.
+    const tx = contactDb.transaction('contacts', 'readwrite');
+  
+    // Open up the desired object store.
+    const store = tx.objectStore('contacts');
+  
+    // Use the .add() method on the store and pass in the content.
+    const request = store.add({ name: name, email: email, phone: phone, profile: profile });
+  
+    // Get confirmation of the request.
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+  }
--- client/src/js/index.js
+++ client/src/js/index.js
@@ -2,7 +2,7 @@ import "./form";
 import "./submit";
 import { Tooltip, Toast, Popover } from "bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { initdb } from "./database";
+import { initdb, getDB, postDb } from "./database";
 import "../css/index.css";
 
 import Logo from '../images/logo.png';
@@ -11,6 +11,9 @@ import Dog from '../images/dog.png';
 
 window.addEventListener('load', function() {
     initdb();
+    getDB();
+    postDb("Lernantino", "learnantino@test.com", 8186601234, "Bear");
+    getDB();
     document.getElementById('logo').src = Logo;
     document.getElementById('bearThumbnail').src = Bear;
     document.getElementById('dogThumbnail').src = Dog;
